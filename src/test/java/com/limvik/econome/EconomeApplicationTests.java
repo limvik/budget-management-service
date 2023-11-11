f248e8bf331837406ee96066b5be787211698a3c
Seongguk Jeong <seongguk.dev@gmail.com>
2023-11-11 12:21:48 +0900

    test: 정상적인 토큰 발급
    정상적으로 토큰이 생성되는지 확인하기 위한 테스트입니다.

--- src/test/java/com/limvik/econome/EconomeApplicationTests.java
+++ src/test/java/com/limvik/econome/EconomeApplicationTests.java
@@ -1,6 +1,9 @@
 package com.limvik.econome;
 
+import com.limvik.econome.domain.user.entity.User;
 import com.limvik.econome.global.config.JwtConfig;
+import com.limvik.econome.global.security.jwt.provider.JwtProvider;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,16 +20,34 @@ class EconomeApplicationTests {
 	@Autowired
 	JwtConfig jwtConfig;
 
+	@Autowired
+	JwtProvider jwtProvider;
+
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
+	@DisplayName("JWT 설정 기본값 정상 로딩")
 	void jwtConfigDataLoads() {
 		assertThat(jwtConfig.getIssuer()).isNotNull();
 		assertThat(jwtConfig.getKey()).isNotNull();
 		assertThat(jwtConfig.getAccessTokenExpirationMinutes()).isNotNull();
 		assertThat(jwtConfig.getRefreshTokenExpirationDays()).isNotNull();
+	}
+
+	@Test
+	@DisplayName("token 정상 발급")
+	void getTokens() {
+		var user = User.builder().id(1L).build();
+		String accessToken = jwtProvider.generateAccessToken(user);
+		String refreshToken = jwtProvider.generateRefreshToken(user);
+
+		assertThat(accessToken).isNotNull();
+		assertThat(refreshToken).isNotNull();
+
+		assertThat(jwtProvider.parse(accessToken).getPayload().getSubject()).isEqualTo("1");
+		assertThat(jwtProvider.parse(refreshToken).getPayload().getSubject()).isEqualTo("1");
 	}
 
 }
