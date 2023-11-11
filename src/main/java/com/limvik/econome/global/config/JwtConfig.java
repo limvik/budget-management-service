0bda87538954b97113d3439b4fe07b373ee237d2
Seongguk Jeong <seongguk.dev@gmail.com>
2023-11-11 11:44:26 +0900

    chore: JWT 기본 설정값 추가
    JWT 발급을 위한 issuer, key, 그리고 access, refresh token 에 대한 만료 시간 설정값을 추가하였습니다.

---
+++ src/main/java/com/limvik/econome/global/config/JwtConfig.java
@@ -0,0 +1,19 @@
+package com.limvik.econome.global.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties("jwt")
+public class JwtConfig {
+
+    private String issuer;
+    private String key;
+    private Long accessTokenExpirationMinutes;
+    private Long refreshTokenExpirationDays;
+
+}
