b27aa81f156b18d5d5fb4abd0910b5e115989a7f
Seongguk Jeong <seongguk.dev@gmail.com>
2023-11-11 11:45:15 +0900

    test: JWT 기본 설정값 불러오기
    JWT 발급을 위한 기본 설정값이 정상적으로 불러와지는지 테스트합니다.

--- src/test/java/com/limvik/econome/EconomeApplicationTests.java
+++ src/test/java/com/limvik/econome/EconomeApplicationTests.java
@@ -1,15 +1,32 @@
 package com.limvik.econome;
 
+import com.limvik.econome.global.config.JwtConfig;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("integration")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class EconomeApplicationTests {
+
+	@Autowired
+	JwtConfig jwtConfig;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void jwtConfigDataLoads() {
+		assertThat(jwtConfig.getIssuer()).isNotNull();
+		assertThat(jwtConfig.getKey()).isNotNull();
+		assertThat(jwtConfig.getAccessTokenExpirationMinutes()).isNotNull();
+		assertThat(jwtConfig.getRefreshTokenExpirationDays()).isNotNull();
 	}
 
 }
