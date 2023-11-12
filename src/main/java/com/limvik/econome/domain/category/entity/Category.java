67aaff5e04a456689c681b173cd8ed591d36fb03
Seongguk Jeong <seongguk.dev@gmail.com>
2023-11-12 19:12:08 +0900

    feat: 도메인 객체 추가 및 관계 설정
    BudgetPlan 객체를 추가하고, Category, User와의 관계를 설정하였습니다.

--- src/main/java/com/limvik/econome/domain/category/entity/Category.java
+++ src/main/java/com/limvik/econome/domain/category/entity/Category.java
@@ -1,11 +1,14 @@
 package com.limvik.econome.domain.category.entity;
 
+import com.limvik.econome.domain.budgetplan.entity.BudgetPlan;
 import com.limvik.econome.domain.category.enums.BudgetCategory;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Builder
 @Getter
@@ -23,4 +26,6 @@ public class Category {
     @Enumerated(EnumType.STRING)
     private BudgetCategory name;
 
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<BudgetPlan> budgetPlans;
 }
